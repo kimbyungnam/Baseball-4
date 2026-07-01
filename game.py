bb4eaaba4808eabe0146d4c6378b2311459b936f
kimbyungnam <kimbyungnam@ajou.ac.kr>
2026-07-01 14:47:34 +0900

    [feature] 3

--- game.py
+++ game.py
@@ -1,7 +1,10 @@
 class Game:
-    def guess(self, guess_number):
+    def guess(self, guess_number: str):
         if guess_number is None:
             raise TypeError()
 
         if len(guess_number) != 3:
+            raise TypeError()
+
+        if not guess_number.isdigit():
             raise TypeError()
